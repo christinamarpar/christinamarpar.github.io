bb5e93c49f48d8a52cf5a76e7d842e61b89390bd
Christina Park <cmp2dz@virginia.edu>
2020-07-26 11:40:01 -0400

    Update js to Youtube India version

--- js/contact_submitToAPI.js
+++ js/contact_submitToAPI.js
@@ -2,37 +2,28 @@ function submitToAPI(e) {
     e.preventDefault();
     var URL = "https://uy3jnnjkr4.execute-api.us-east-1.amazonaws.com/prod/rest/contact";
 
-         var Namere = /[A-Za-z]{1}[A-Za-z]/;
-         if (!Namere.test($("#name-input").val())) {
+    var name = document.getElementById("name-input").value;
+    var email = document.getElementById("#email-input").value;
+    var subject = document.getElementById("#subject-input").value;
+    var message = document.getElementById("#message-input").value;
+
+         var nameRE = /[A-Za-z]{1}[A-Za-z]/;
+         if (!nameRE.test(name.val())) {
             alert ("Name cannot be less than 2 characters");
             return;
          }
 
-         if ($("#email-input").val()=="") {
-            alert ("Please enter your email address");
-            return;
-         }
-
-         var reemail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-         if (!reemail.test($("#email-input").val())) {
-             alert ("Please enter valid email address");
+         var emailRE = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+         if (!emailRE.test(email.val())) {
+             alert ("Please enter a valid email address");
              return;
          }
 
-         if ($("#subject-input").val()=="") {
-            alert ("Please enter a subject");
-            return;
+         if (name=="" || email=="" || subject=="" || message=="")
+         {
+             alert("Please fill all required fields");
+             return false;
          }
-
-         if ($("#message-input").val()=="") {
-            alert ("Please enter a message");
-            return;
-         }
-
-    var name = $("#name-input").val();
-    var email = $("#email-input").val();
-    var subject = $("#subject-input").val();
-    var message = $("#message-input").val();
     
     var data = {
        name : name,
@@ -41,24 +32,43 @@ function submitToAPI(e) {
        message : message
      };
 
-    $.ajax({
-      type: "POST",
-      url: "https://uy3jnnjkr4.execute-api.us-east-1.amazonaws.com/prod/rest/contact",
-      //url : "https://uy3jnnjkr4.execute-api.us-east-1.amazonaws.com/prod",
-      dataType: "json",
-      crossDomain: "true",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-      cache: false,
-      
-      success: function () {
-        // clear form and show a success message
-        alert("Successful");
-        document.getElementById("contact-form").reset();
-        location.reload();
-      },
-      error: function () {
-        // show an error message
-        alert("UnSuccessful");
-      }});
+     var xmlhttp = new XMLHttpRequest();
+     xmlhttp.open("POST", URL);
+     xmlhttp.setRequestHeader("Content-Type", "application/json");
+     xmlhttp.send(JSON.stringify(data));
+     xmlhttp.onreadystatechange = function() {
+     if (xmlhttp.readyState === 4) {
+      //   var response = JSON.parse(xmlhttp.responseText);
+        if (xmlhttp.status === 200 ) {
+           console.log('successful');
+           document.getElementById("contact-form").innerHTML = "<h1>Thank you for your message/feedback<br>our team will get back to you soon!</h1>";
+         } else {
+            console.log('failed');
+        }
+     }
   }
+  
+  document.getElementById('contact-form').reset();
+    
+  }
+
+//     $.ajax({
+//       type: "POST",
+//       url: "https://uy3jnnjkr4.execute-api.us-east-1.amazonaws.com/prod/rest/contact",
+//       dataType: "json",
+//       crossDomain: "true",
+//       contentType: "application/json; charset=utf-8",
+//       data: JSON.stringify(data),
+//       cache: false,
+      
+//       success: function () {
+//         // clear form and show a success message
+//         alert("Successful");
+//         document.getElementById("contact-form").reset();
+//         location.reload();
+//       },
+//       error: function () {
+//         // show an error message
+//         alert("UnSuccessful");
+//       }});
+//   }
